1224f1e899267602997f13f9b9eac6a30cadf0d2
DeZin7 <marcusandre77@icloud.com>
2023-11-28 21:31:23 -0800

    Add the aws vpc.

---
+++ AWS-VPC/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.60.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "dezin7-bucket"
+    key    = "aws-vpc/terraform.tfstate"
+    region = "us-west-2"
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+
+  default_tags {
+    tags = {
+      owner      = "dezin7"
+      managed-by = "terraform"
+    }
+  }
+}
