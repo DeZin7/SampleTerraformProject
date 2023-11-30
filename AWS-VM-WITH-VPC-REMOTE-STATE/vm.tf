f31e4db15f3ed287c69b0962013aff32dd95215e
DeZin7 <marcusandre77@icloud.com>
2023-11-29 23:46:15 -0800

    Creates an EC2 instance.

---
+++ AWS-VM-WITH-VPC-REMOTE-STATE/vm.tf
@@ -0,0 +1,17 @@
+resource "aws_key_pair" "key" {
+  key_name   = "aws-key"
+  public_key = file("./aws-key.pub")
+}
+
+resource "aws_instance" "vm" {
+  ami                         = "ami-0efcece6bed30fd98"
+  instance_type               = "t2.micro"
+  key_name                    = aws_key_pair.key.key_name
+  subnet_id                   = data.terraform_remote_state.vpc.outputs.subnet_id
+  vpc_security_group_ids      = [data.terraform_remote_state.vpc.outputs.security_group]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "vm-terraform"
+  }
+}
